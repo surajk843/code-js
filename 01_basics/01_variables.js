14f182753543a8102d3dc9360bda83276aec17b4
surajk843 <164765494+surajk843@users.noreply.github.com>
2026-05-18 10:21:25 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountID = 12345
+let accountEmail =  "hhtc@gmail.com"
+var accountPassword = "123456"
+accountCity = "Banglore"
+
+//accountID = 2
+accountEmail = "KKrtmc@google.com"
+accountPassword = "45678"
+accountCity = "mumbai"
+console.table([accountID,accountEmail,accountPassword, accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all Js codes as newer version
+
+console.log(3 + 3)
+console.log("Suraj")
+
+let name = "suraj"
+let age = 26
+let IsLoggedIn = "false"
+
+console.table([name,age,IsLoggedIn])
+
+/*
+numbers => 2 raise to 52
+bigint => b9ig numbers
+string => ""
+boolean => true or false
+null => standalone value
+undefined => if not assigned any value to the variables
+Symbol => unique
+
+object 
+*/
+console.log(typeof null);
