509ef8d33b7971d9838edba41b1f1667fbd0a9c9
surajk843 <164765494+surajk843@users.noreply.github.com>
2026-05-18 13:09:11 +0000

    comparision done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,29 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 == 1);
+// console.log(2 <= 1);
+// console.log(2 != 1);
+// console.log(2 < 1);
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+// false
+// false
+// true
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+// false
+// false
+// false
+// dono write this type of codes mentioned above
+
+//best for comparision is ===
+console.log(15 === "15");
+//false
+console.log(2 === 2);
+//true
