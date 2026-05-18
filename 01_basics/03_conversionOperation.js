4daf67df910db745201d63b33a93561dc871381e
surajk843 <164765494+surajk843@users.noreply.github.com>
2026-05-18 11:35:56 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = undefined
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 /*
@@ -20,7 +20,7 @@ undefined => undefined value = NaN
 let IsLoggedIn = undefined
 
 let IsbooleanLoggedIn = Boolean(IsLoggedIn)
-console.log(IsbooleanLoggedIn);
+//console.log(IsbooleanLoggedIn);
 /*
 number < 0 ; string => true
 empty string "" => false
@@ -29,9 +29,49 @@ empty string "" => false
 
 let someNumber = 45
 let someString = String(someNumber)
-console.log(someString);
-console.log(typeof someString);
+// console.log(someString);
+// console.log(typeof someString);
 
 /*
 string => all types in strinf and gives all the value same 
 */
+/**************  Operations  *********************/
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+ // all pos => neg but no neg => pos
+
+//  console.log(2+2)
+//  console.log(2-2)
+//  console.log(2*2)
+//  console.log(2**3)
+//  console.log(2/2)
+//  console.log(2%3)
+ 
+
+let str1 = "Hello"
+let str2 = " World"
+let str3 = str1 + str2
+//console.log(str3);
+
+// console.log(2+"2")
+// console.log("2"+2)
+// console.log(1+"2"+3)
+// console.log(1+2+"3")
+// answers 
+// 22
+// 22
+// 123
+// 33
+console.log(true)
+// true
+console.log(+true)
+//1
+
+let gameCounter = 100 
+// console.log(gameCounter);
+// console.log(++gameCounter)
+// 100
+// 101
+
